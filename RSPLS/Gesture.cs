21e0f08ff71c4d09a24ac08f769d9a193ada2548
Pascal Pascarella <pascalpascarella86@gmail.com>
2020-09-18 11:49:12 -0500

    Add Gesture class to organize lists

---
+++ RSPLS/Gesture.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RSPLS
+{
+	class Gesture
+	{
+		// Member Variables
+		public string rock;
+		public string scissors;
+		public string paper;
+		public string lizard;
+		public string spock;
+		public List<string> gestures;
+		public List<int> gestureValues;
+
+		// Constructor
+		public Gesture()
+		{
+
+		}
+
+		// Member Methods
+
+
+	}
+}
